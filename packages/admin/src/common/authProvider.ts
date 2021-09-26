15d58e2bbdeec3a40a2991e6e9f6b59781458801
AntePost <is1213791@gmail.com>
2021-09-26 23:01:06 +0300

    admin: lint

--- packages/admin/src/common/authProvider.ts
+++ packages/admin/src/common/authProvider.ts
@@ -1,4 +1,4 @@
-export default {
+const authProvider = {
   // called when the user attempts to log in
   login: async ({ username }: { username: string }) => {
     localStorage.setItem('username', username)
@@ -27,3 +27,5 @@ export default {
   // called when the user navigates to a new location, to check for permissions / roles
   getPermissions: async () => await Promise.resolve()
 }
+
+export default authProvider
--- packages/admin/src/components/Dashboard.tsx
+++ packages/admin/src/components/Dashboard.tsx
@@ -1,9 +1,11 @@
 import { Card, CardContent, CardHeader } from '@material-ui/core'
 import { ReactElement } from 'react'
 
-export default (): ReactElement<any, any> => (
+const Dashboard = (): ReactElement<any, any> => (
   <Card>
     <CardHeader title='Welcome to the administration' />
     <CardContent>Lorem ipsum sic dolor amet...</CardContent>
   </Card>
 )
+
+export default Dashboard
