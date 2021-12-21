e1b198a2916940cba8eaa62b2c32b4c3785c19d1
Natalya <natasha-vd@yandex.ru>
2021-12-21 13:27:43 +0300

    frontend: change page index

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,59 +1,17 @@
 import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
+import Layout, { siteTitle } from '../src/components/layouts/Layout'
 import Link from 'next/link'
-import Date from '../components/date'
-import { GetStaticProps } from 'next'
 
-export const getStaticProps: GetStaticProps = async () => {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
-
-export default function Home ({
-  allPostsData
-}: {
-  allPostsData: Array<{
-    date: string
-    title: string
-    id: string
-  }>
-}): JSX.Element {
+export default function Home (): JSX.Element {
   return (
-    <Layout home>
+    <Layout>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles['headingMd']}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
-        </p>
-      </section>
-      {/* eslint-disable-next-line @typescript-eslint/restrict-template-expressions */}
-      <section className={`${utilStyles['headingMd']} ${utilStyles['padding1px']}`}>
-        <h2 className={utilStyles['headingLg']}>Blog</h2>
-        <ul className={utilStyles['list']}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles['listItem']} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles['lightText']}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-        <Link href="/form">
-            <a>Форма</a>
+      <section className='container'>
+        <h2>Главная</h2>
+        <Link href='/form'>
+          <a>Форма</a>
         </Link>
       </section>
     </Layout>
