67526c4915dacc3ee50cbc4f7c2068c2ffea24b2
lgmarch <lmarch@yandex.ru>
2022-02-05 11:13:38 +0300

    Creating a seeder ApartmentSeeder.ts

---
+++ packages/backend/database/seeders/ApartmentSeeder.ts
@@ -0,0 +1,8 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import { ApartmentsFactory } from 'Database/factories/ApartmentsFactory'
+
+export default class ApartmentSeeder extends BaseSeeder {
+  public async run (): Promise<void> {
+    await ApartmentsFactory.createMany(2)
+  }
+}
