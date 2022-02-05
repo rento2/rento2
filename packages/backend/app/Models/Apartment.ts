bf471fa8e838e14d64e626e77777dc6b77450aa8
lgmarch <lmarch@yandex.ru>
2022-02-05 10:49:12 +0300

    Creating a model Apartment.ts

---
+++ packages/backend/app/Models/Apartment.ts
@@ -0,0 +1,137 @@
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from 'luxon'
+import Term from 'Contracts/enums/term'
+
+export default class Apartment extends BaseModel {
+  @column({ isPrimary: true })
+  public id!: number
+
+  @column()
+  public type!: Term
+
+  @column()
+  public isActive!: boolean
+
+  @column()
+  public name!: string
+
+  @column()
+  public latinName!: string
+
+  @column()
+  public description!: string
+
+  @column()
+  public bnovoId!: number
+
+  @column()
+  public novoId!: number
+
+  @column()
+  public price!: number
+
+  @column()
+  public pricePerMonth!: number
+
+  @column()
+  public discount!: number
+
+  @column()
+  public commission!: number
+
+  @column()
+  public utilityBills!: number
+
+  @column()
+  public securityDepositShort!: number
+
+  @column()
+  public securityDepositLong!: number
+
+  @column()
+  public roomsNum!: number
+
+  @column()
+  public storey!: number
+
+  @column()
+  public totalStoreys!: number
+
+  @column()
+  public area!: number
+
+  @column()
+  public kitchenArea: any
+
+  @column()
+  public distanceFromCenter!: number
+
+  @column()
+  public admArea!: string
+
+  @column()
+  public district!: string
+
+  @column()
+  public sellingPoint!: string
+
+  @column()
+  public geoCoordinateX!: string
+
+  @column()
+  public geoCoordinateY!: string
+
+  @column()
+  public subwayStation!: string
+
+  @column()
+  public timeToSubway!: number
+
+  @column()
+  public repairs!: number
+
+  @column()
+  public purity!: number
+
+  @column()
+  public location!: number
+
+  @column()
+  public priceQuality!: number
+
+  @column()
+  public totalRating!: number
+
+  @column()
+  checkInStart!: DateTime
+
+  @column()
+  checkInEnd!: DateTime
+
+  @column()
+  checkOutEnd!: DateTime
+
+  @column()
+  smokingAllowed!: boolean
+
+  @column()
+  partying_allowed!: boolean
+
+  @column()
+  children_allowed!: boolean
+
+  @column()
+  pets_allowed!: boolean
+
+  @column()
+  maxAdults!: number
+
+  @column()
+  max_children!: number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt!: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt!: DateTime
+}
