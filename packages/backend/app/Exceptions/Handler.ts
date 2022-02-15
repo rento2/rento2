23872a1fdbd6fe1f8d4626a9934a2634a063d4fa
lgmarch <lmarch@yandex.ru>
2022-02-15 07:50:20 +0300

    Валидация данных со фронта.

--- packages/backend/app/Exceptions/Handler.ts
+++ packages/backend/app/Exceptions/Handler.ts
@@ -1,23 +1,19 @@
-/*
-|--------------------------------------------------------------------------
-| Http Exception Handler
-|--------------------------------------------------------------------------
-|
-| AdonisJs will forward all exceptions occurred during an HTTP request to
-| the following class. You can learn more about exception handling by
-| reading docs.
-|
-| The exception handler extends a base `HttpExceptionHandler` which is not
-| mandatory, however it can do lot of heavy lifting to handle the errors
-| properly.
-|
-*/
-
 import Logger from '@ioc:Adonis/Core/Logger'
 import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import creatingErrorMsg from 'App/Controllers/creatingErrorMsg'
 
 export default class ExceptionHandler extends HttpExceptionHandler {
   constructor () {
     super(Logger)
+  }
+
+  public async handle (error: any, ctx: HttpContextContract): Promise<any> {
+    if (error.code === 'E_VALIDATION_FAILURE') {
+      return ctx.response.status(422)
+        .send(creatingErrorMsg('VALIDATION_FAILURE', error.messages.errors))
+    }
+
+    return await super.handle(error, ctx)
   }
 }
---
+++ packages/backend/app/Validators/ValidatorMessages.ts
@@ -0,0 +1,5 @@
+export default class ValidatorMessages {
+  public messages = {
+    // required: "{{ field }} is required",
+  }
+}
