edafcc4a0940499d193a18414f4229336f11dcb9
AndMuruGvart <shshk777@yandex.ru>
2022-03-15 07:48:27 +0500

    3rd commit

--- packages/frontend/src/components/modules/CalculationForm/CalculationDropdown/CalculationDropdown.tsx
+++ packages/frontend/src/components/modules/CalculationForm/CalculationDropdown/CalculationDropdown.tsx
@@ -1,87 +1,92 @@
 /* eslint-disable react/jsx-closing-tag-location */
 
-import { Select } from 'antd'
 // import React from 'react'
 import styles from './CalculationDropdown.module.scss'
-import 'antd/dist/antd.css'
-import GoWalkingIcon from '@elements/icons/GoWalkingIcon'
-import GoDrivingIcon from '@elements/icons/GoDrivingIcon'
+import Select from 'react-select'
+import IconGoWalking from '@elements/icons/IconGoWalking'
+import IconGoDriving from '@elements/icons/IconGoDriving'
 
-const { Option } = Select
 
-// function handleChange (value: string) {
-//   console.log(`selected ${value}`)
-// }
 
-const minutes: string[] = ['Выбрать', '5 минут', '10 минут', '15 минут', '20 минут', '25+ минут']
+const options = [
+  { value: 'Choose', label: 'Выбрать' },
+  { value: '5 minutes', label: '5 минут' },
+  { value: '10 minutes', label: '10 минут' },
+  { value: '15 minutes', label: '15 минут' },
+  { value: '20 minutes', label: '20 минут' },
+  { value: '25+ minutes', label: '25+ минут' }
+]
 
 export function CalculationDropdown (): JSX.Element {
+
+  const style = {
+    control: (base: any) => ({
+      ...base,
+        marginLeft:"10px",
+        border: 0,
+        boxShadow: "none",
+        fontSize: "18px",
+        fontStyle: "normal",
+        fontWeight: "500",
+        lineHeight: "22px",
+
+    }),
+      option: (base: any, state:any) => ({
+        ...base,
+        paddingLeft:"22px",
+        fontSize: "16px",
+        fontStyle: "normal",
+        fontWeight: "500",
+        lineHeight: "20px",
+        backgroundColor: state.isSelected ? 'transparent' : 'transparent',
+        color: state.isSelected ? '#6f57eb' : '#7F8085',
+      }),
+      singleValue: (base: any) => ({
+        ...base,
+        color: "#7F8085"
+      }),
+      input: (base: any) => ({
+        ...base,
+        color: "#fff"
+      }),
+      container: (base: any) => ({
+        ...base,
+        flex: 2
+      })
+  };
   return (
     <div className={ styles['container'] }>
-      <Select
-        defaultValue="Выбрать"
-        showArrow={ false }
-      >
-        {
-          minutes.map(min =>
-            (<Option key={ min }
-              value={ min }
-            >
-              {min}
-            </Option>))
-        }
+            <Select
+              styles={style}
+              defaultValue={options[0]}
+              components={{ DropdownIndicator: null }}
+              options={options} />
 
-      </Select>
+          <div className={ styles['route'] }>
+            <label>
+              <input className={ styles['input-hidden'] }
+                defaultChecked={ true }
+                name="route"
+                type="radio"
+                value="man-route"
+              />
 
-      {/* <Select
-        defaultValue="Выбрать"
-        showArrow={ false }
-        onChange={ handleChange }
-      >
-        <Option value="Choose">
-          Выбрать
-        </Option>
-        <Option value="5 minutes">
-          5 минут
-        </Option>
-        <Option value="10 minutes">
-          10 минут
-        </Option>
-        <Option value="15 minutes">
-          15 минут
-        </Option>
-        <Option value="20 minutes">
-          20 минут
-        </Option>
-        <Option value="25 minutes">
-          25+ минут
-        </Option>
-      </Select> */}
-      <div className={ styles['route'] }>
-        <label>
-          <input className={ styles['input-hidden'] }
-            defaultChecked={ true }
-            name="route"
-            type="radio"
-            value="man-route"
-          />
+              <div className={ styles['img-route'] }>
+                <IconGoWalking />
+              </div>
+            </label>
+            <label>
+              <input className={ styles['input-hidden'] }
+                name="route"
+                type="radio"
+                value="bus-route"
+              />
+              <div className={ styles['img-route'] }>
+                <IconGoDriving />
+              </div>
 
-          <div className={ styles['img-route'] }>
-            <GoWalkingIcon />
+            </label>
           </div>
-        </label>
-        <label>
-          <input className={ styles['input-hidden'] }
-            name="route"
-            type="radio"
-            value="bus-route"
-          />
-          <div className={ styles['img-route'] }>
-            <GoDrivingIcon />
-          </div>
-
-        </label>
-      </div>
     </div>
   )
 }
