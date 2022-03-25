bcfea23fea866971c4bc1f96f92ece28ccf42543
Natalya <natasha-vd@yandex.ru>
2022-03-25 08:50:45 +0300

    frontend: add fonts global space Next.js

--- packages/frontend/pages/_document.tsx
+++ packages/frontend/pages/_document.tsx
@@ -4,7 +4,14 @@ class MyDocument extends Document {
   override render (): JSX.Element {
     return (
       <Html lang='ru'>
-        <Head />
+        <Head>
+          <link href="https://fonts.googleapis.com"
+            rel="preconnect"
+          />
+          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Prosto+One&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
