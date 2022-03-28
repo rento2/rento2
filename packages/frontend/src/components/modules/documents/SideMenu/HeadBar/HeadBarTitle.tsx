b1f99ad92f3782217910e0c20798771fe4441f81
BrynzevDmitrii <Elf_happy@mail.ru>
2022-03-28 23:19:54 +0300

    Dinamik mobile title

--- packages/frontend/src/components/modules/documents/SideMenu/HeadBar/HeadBarTitle.tsx
+++ packages/frontend/src/components/modules/documents/SideMenu/HeadBar/HeadBarTitle.tsx
@@ -1,12 +1,36 @@
+import { useRouter } from 'next/dist/client/router'
+import { useEffect, useState } from 'react'
 import classNames from 'classnames'
+
 import style from './HeadBar.module.scss'
 
-const HeadBarTitle = ({ btnOnClick }: {btnOnClick: () => any}): JSX.Element => {
+const HeadBarTitle = ({ btnOnClick, isOpen }: {btnOnClick: () => void, isOpen: boolean}): JSX.Element => {
+  const [titleText, setTitleText] = useState('')
+  const router = useRouter()
+  function headTitle (router: string): string {
+    switch (router) {
+      case '/docs/faq':
+        return 'частые вопросы'
+      case '/docs/privacy':
+        return 'Политика обработки данных'
+      case '/docs/rules':
+        return 'Пользовательское соглашение'
+      case '/docs/agreements':
+        return 'Образцы документов'
+      default:
+        return 'нет такой страницы'
+    }
+  }
+
+  useEffect(() => {
+    setTitleText(headTitle(router.pathname))
+  }, [router.pathname])
+
   return (
-    <button className={ classNames(style['title'], style['title__text']) }
+    <button className={ classNames(style['title'], style['title__text'], isOpen ? style['active'] : '') }
       onClick={ btnOnClick }
     >
-      Частые вопросы
+      {titleText}
     </button>
   )
 }
