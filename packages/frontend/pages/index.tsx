c8462d4f61de00ed9c140a11f7be00a14b393623
Natalya <natasha-vd@yandex.ru>
2022-03-28 10:58:10 +0300

    front: FS change pages

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,25 +1,7 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../src/components/layouts/Layout'
-import Link from 'next/link'
+import { HomePage } from '@pages-fs/home/HomePage'
 
 export default function Home (): JSX.Element {
   return (
-    <Layout>
-      <Head>
-        <title>
-          {siteTitle}
-        </title>
-      </Head>
-      <section className='container'>
-        <h2>
-          Главная
-        </h2>
-        <Link href='/form'>
-          <a>
-            Форма
-          </a>
-        </Link>
-      </section>
-    </Layout>
+    <HomePage />
   )
 }
