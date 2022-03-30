5d6509ea6bd4efe4b3824a6fcffc8a67b4cb88f2
Luka Nikitin <lukentuichennel@gmail.com>
2022-03-30 21:06:12 +0200

    review controller

--- packages/backend/database/seeders/ReviewSeeder.ts
+++ packages/backend/database/seeders/ReviewSeeder.ts
@@ -1,7 +1,8 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import { ReviewsFactory } from 'Database/factories/ReviewsFactory'
 
-export default class ApartmentSeeder extends BaseSeeder {
+export default class ReviewSeeder extends BaseSeeder {
+  public static developmentOnly = true
   public async run (): Promise<void> {
     await ReviewsFactory.with('apartment').createMany(20)
   }
