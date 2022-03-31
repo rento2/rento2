ee56240c7ff7bbb9587b4f7b3fccb40c0d1efb70
Natalya <natasha-vd@yandex.ru>
2022-03-31 16:27:41 +0300

    front: change public API shared/ui

--- packages/frontend/srcFs/shared/ui/index.ts
+++ packages/frontend/srcFs/shared/ui/index.ts
@@ -2,3 +2,6 @@
 export * from './icons'
 export * from './Layout'
 export * from './buttons/ButtonGeneral'
+export * from './modals/modal-base/ModalBase'
+export * from './modals/modal-swipe/ModalSwipe'
+export * from './modals/hooks/useModal'
