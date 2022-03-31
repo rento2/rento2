4319b69b5bc05775d77d06759d8fa2505c6a2f61
Natalya <natasha-vd@yandex.ru>
2022-03-31 16:45:27 +0300

    front: change widget footer

--- packages/frontend/srcFs/widgets/footer/ui/Footer.tsx
+++ packages/frontend/srcFs/widgets/footer/ui/Footer.tsx
@@ -1,61 +1,71 @@
 import classNames from 'classnames'
 import { IconVk, IconTelegram, IconWhatsapp, ButtonGeneral } from '@shared/ui'
-import { DocsItem } from './docsItem/DocsItem'
-import { SocialItem } from './socialItem/SocialItem'
+import { DocsItem } from './docs-item/DocsItem'
+import { SocialItem } from './social-item/SocialItem'
 import { dataDocsGroup1, dataDocsGroup2 } from '@widgets/footer/model/dataFooter'
 import styles from './Footer.module.scss'
+import { useWindowDimensions } from '@shared/lib'
 
 export const Footer = (): JSX.Element => {
+  const { widthWindow } = useWindowDimensions()
+
   return (
     <footer className={ styles['footer'] }>
-      <div className={ classNames(styles['footer__inner'], 'container-base', 'flex-s-b') }>
-        <ul>
-          {dataDocsGroup1.map((item, i) => (<DocsItem key={ i }
-            hrefProps={ item.hrefProps }
-            textProps={ item.textProps }
-          />))}
-        </ul>
-
-        <ul>
-          {dataDocsGroup2.map((item, i) => (<DocsItem key={ i }
-            hrefProps={ item.hrefProps }
-            textProps={ item.textProps }
-          />))}
-        </ul>
-
-        <div>
-          <p className={ styles['footer__title-socials'] }>
-            Контакты
-          </p>
-          <ul className={ classNames(styles['footer__list-socials'], 'flex-s-b-c') }>
-            <SocialItem
-              hrefProps='#'
-              iconProps={ <IconTelegram /> }
-              titleProps='Telegram'
-            />
-            <SocialItem
-              hrefProps='#'
-              iconProps={ <IconWhatsapp /> }
-              titleProps='Whatsapp'
-            />
-            <SocialItem
-              hrefProps='#'
-              iconProps={ <IconVk /> }
-              titleProps='Vk'
-            />
+      <div className={ classNames(styles['footer__inner'], 'container', 'flex-s-b') }>
+        <div className={ classNames(styles['footer__box'], 'flex-s-b') }>
+          <ul>
+            {dataDocsGroup1.map((item, i) => (<DocsItem key={ i }
+              hrefProps={ item.hrefProps }
+              textProps={ item.textProps }
+            />))}
           </ul>
+          {
+          widthWindow > 1023 && (
+            <ul>
+              {dataDocsGroup2.map((item, i) => (<DocsItem key={ i }
+                hrefProps={ item.hrefProps }
+                textProps={ item.textProps }
+              />))}
+            </ul>)
+          }
+          <div>
+            <p className={ styles['footer__title-socials'] }>
+              Контакты
+            </p>
+            <ul className={ classNames(styles['footer__list-socials'], 'flex-s-b-c') }>
+              <SocialItem
+                hrefProps='#'
+                iconProps={ <IconTelegram /> }
+                titleProps='Telegram'
+              />
+              <SocialItem
+                hrefProps='#'
+                iconProps={ <IconWhatsapp /> }
+                titleProps='Whatsapp'
+              />
+              <SocialItem
+                hrefProps='#'
+                iconProps={ <IconVk /> }
+                titleProps='Vk'
+              />
+            </ul>
+          </div>
         </div>
 
         <ul className={ classNames(styles['footer__btn-group'], 'flex-s-b') }>
           <li>
-            <ButtonGeneral size='xs'
+            <ButtonGeneral classProps={ classNames(styles['footer__btn']) }
+              font={ widthWindow > 479 ? 's' : 'm' }
+              height={ widthWindow > 479 ? '40' : '44' }
               text='Сдать жильё'
               type='button'
             />
           </li>
           <li>
-            <ButtonGeneral grade='secondary'
-              size='xs'
+            <ButtonGeneral classProps={ classNames(styles['footer__btn']) }
+              font={ widthWindow > 479 ? 's' : 'm' }
+              grade='secondary'
+              height={ widthWindow > 479 ? '40' : '44' }
               text='Заказать звонок'
               type='button'
             />
@@ -63,7 +73,7 @@ export const Footer = (): JSX.Element => {
         </ul>
       </div>
       <div className={ styles['footer__address'] }>
-        <div className={ classNames(styles['footer__address-inner'], 'container-base', 'flex-s-b-c') }>
+        <div className={ classNames(styles['footer__address-inner'], 'container', 'flex-s-b-c') }>
           <p>
             ООО «Ренто»; ИНН 9709072210 КПП 770901001; 105120, г. Москва
           </p>
