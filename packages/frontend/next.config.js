ac43e6e211d0f8ce30e5bc6d83c37f6a94b7d891
Natalya <natasha-vd@yandex.ru>
2022-03-31 17:17:06 +0300

    front: change next.config

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -5,9 +5,9 @@ const nextConfig = {
   // workaround as per https://github.com/vercel/next.js/issues/11368#issuecomment-894822002
   sassOptions: {
     prependData: `
-@use 'src/styles/core/colors' as *;
-@use 'src/styles/core/typography' as *;
-@use 'src/styles/core/breakpoints' as *;
+@use 'src/app/styles/core/colors' as *;
+@use 'src/app/styles/core/typography' as *;
+@use 'src/app/styles/core/breakpoints' as *;
 `
   }
 }
