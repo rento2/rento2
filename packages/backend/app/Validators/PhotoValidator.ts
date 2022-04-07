b8a4413ff7650e4f1b009b18b76f828535211f24
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-04-07 19:50:22 +0300

    fix: fix remarks

--- packages/backend/app/Validators/PhotoValidator.ts
+++ packages/backend/app/Validators/PhotoValidator.ts
@@ -1,21 +1,4 @@
-import { rules, schema } from '@ioc:Adonis/Core/Validator'
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-
-export class PhotoValidator {
-  constructor (protected ctx: HttpContextContract) {}
-
-  public schema = schema.create({
-    id: schema.number([rules.unsigned()]),
-
-    apartment_id: schema.number([
-      rules.exists({ table: 'apartments', column: 'id' }),
-    ]),
-
-    link: schema.file(),
-  })
-
-  public messages = {}
-}
+import { schema } from '@ioc:Adonis/Core/Validator'
 
 export const photoSchema = schema.create({
   image: schema.file({
