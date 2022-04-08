594f93afa04eff4b89b79d58335a4bb4d60ae3ac
Luka Nikitin <lukentuichennel@gmail.com>
2022-04-08 06:53:36 +0200

    fix log

--- packages/backend/database/factories/OrdersFactory.ts
+++ packages/backend/database/factories/OrdersFactory.ts
@@ -4,25 +4,22 @@ import { returnRandomFromEnum } from '../../common/helpers/enumService'
 import Pay from '../../common/enums/Pay'
 
 export const OrdersFactory = Factory
-  .define(Order, ({ faker }) => {
-    console.warn(Array(15).fill(faker.datatype.number(50_000)))
-    return {
-      name: faker.name.findName(),
-      email: faker.internet.email(),
-      phone: faker.phone.phoneNumber(),
-      dateFrom: faker.datatype.datetime(),
-      dateTo: faker.datatype.datetime(),
-      fixedPrice: faker.datatype.number(50_000),
-      fixedTotalPrice: faker.datatype.number(100_000),
-      nightsNumber: faker.datatype.number(365),
-      paymentType: returnRandomFromEnum(Pay),
-      prices: Array(5).fill(faker.datatype.number(50_000)),
-      adults: faker.datatype.number(10),
-      children: faker.datatype.number(10),
-      babies: faker.datatype.number(10),
-      pets: faker.datatype.boolean(),
-      paidPart: faker.datatype.number(100),
-      number: faker.random.alphaNumeric(),
-    }
-  })
+  .define(Order, ({ faker }) => ({
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    phone: faker.phone.phoneNumber(),
+    dateFrom: faker.datatype.datetime(),
+    dateTo: faker.datatype.datetime(),
+    fixedPrice: faker.datatype.number(50_000),
+    fixedTotalPrice: faker.datatype.number(100_000),
+    nightsNumber: faker.datatype.number(365),
+    paymentType: returnRandomFromEnum(Pay),
+    prices: Array(5).fill(faker.datatype.number(50_000)),
+    adults: faker.datatype.number(10),
+    children: faker.datatype.number(10),
+    babies: faker.datatype.number(10),
+    pets: faker.datatype.boolean(),
+    paidPart: faker.datatype.number(100),
+    number: faker.random.alphaNumeric(),
+  }))
   .build()
