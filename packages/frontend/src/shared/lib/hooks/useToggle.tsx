0e3c148e833054af2a3a18e314fefeeedb94a24e
Natalya <natasha-vd@yandex.ru>
2022-04-10 12:57:37 +0300

    front: move useModal -> useToggle

--- packages/frontend/src/shared/lib/hooks/useToggle.tsx
+++ packages/frontend/src/shared/lib/hooks/useToggle.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-export const useModal = (): [isShown: boolean, toggle: () => void] => {
+export const useToggle = (): [isShown: boolean, toggle: () => void] => {
   const [isShown, setIsShown] = useState(false)
 
   const toggle = (): void => setIsShown(!isShown)
