be049fb3cf1a7adf3fa40910bb4a83eaa5270726
Luka Nikitin <lukentuichennel@gmail.com>
2022-04-10 15:16:43 +0200

    fix

---
+++ packages/backend/app/Models/User.ts
@@ -0,0 +1,26 @@
+import { DateTime } from 'luxon'
+import { BaseModel, beforeSave, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id!: number
+
+  @column()
+  public email!: string
+
+  @column({ serializeAs: null })
+  public password!: string
+
+  @beforeSave()
+  public static async hashPassword (user: User): Promise<void> {
+    if (user.$dirty.password) {
+      user.password = await Hash.make(user.password)
+    }
+  }
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt!: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt!: DateTime
+}
