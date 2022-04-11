5fd5523c8e0e420fb52426fd6df00d7226d9251a
Luka Nikitin <lukentuichennel@gmail.com>
2022-04-11 09:28:47 +0200

    fix

--- packages/backend/app/Controllers/Http/AuthController.ts
+++ packages/backend/app/Controllers/Http/AuthController.ts
@@ -1,15 +1,18 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { HttpStatusCode } from '../../../common/constants/HttpStatusCode'
 import { creatingErrMsg, creatingOkMsg } from '../../../common/helpers/creatingResponse'
-import { schema } from '@ioc:Adonis/Core/Validator'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import User from 'App/Models/User'
 import Hash from '@ioc:Adonis/Core/Hash'
+import { Exception } from '@adonisjs/core/build/standalone'
 
 export default class AuthController {
   public async login ({ response, request, auth }: HttpContextContract): Promise<void> {
     const payload = await request.validate({
       schema: schema.create({
-        email: schema.string(),
+        email: schema.string([
+          rules.email(),
+        ]),
         password: schema.string(),
       })
     })
@@ -17,7 +20,7 @@ export default class AuthController {
     const foundUser = await User.findBy('email', payload.email)
     if (!foundUser || !await Hash.verify(foundUser.password, payload.password)) {
       return response
-        .status(HttpStatusCode.NotFound)
+        .status(HttpStatusCode.UnprocessableEntity)
         .send(creatingErrMsg('UNABLE_TO_AUTHORIZE'))
     }
 
@@ -35,11 +38,15 @@ export default class AuthController {
       })
     })
 
-    return response
-      .status(HttpStatusCode.OK)
-      .send(creatingOkMsg(
-        await auth.use('jwt').loginViaRefreshToken(payload.refreshToken)
-      ))
+    try {
+      return response
+        .status(HttpStatusCode.OK)
+        .send(creatingOkMsg(
+          await auth.use('jwt').loginViaRefreshToken(payload.refreshToken)
+        ))
+    } catch {
+      throw new Exception('Jwt token is not valid', 422, 'ERR_JWS_INVALID')
+    }
   }
 
   public async logout ({ response, auth }: HttpContextContract): Promise<void> {
