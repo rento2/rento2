0f8c204b29da0244364f873cbc113ed9a2667e15
Slavik <slava123101@gmail.com>
2022-04-13 16:57:44 +0500

    location-link hover/focus added, long-one page added

--- packages/frontend/src/pages-fs/long/LongPage.tsx
+++ packages/frontend/src/pages-fs/long/LongPage.tsx
@@ -1,39 +1,8 @@
 import { Layout } from '@shared/ui'
 import { Header } from '@widgets/header'
 import { Footer } from '@widgets/footer'
-import { LongInfo } from '@widgets/long-info'
 
 const titlePage = 'Снять хорошую квартиру от 6 месяцев &#128156;'
-
-const testAdress = {
-  street: '2й Спассоналивковский пер. 16/2',
-  metro: [
-    {
-      name: 'Бульвар Адмирала Ушакова',
-      time: '9 минут',
-      iconClass: 'icon-light-green'
-    },
-    {
-      name: 'Бульвар Ещё кого-то',
-      time: '15 минут',
-      iconClass: 'icon-blue'
-    }
-  ]
-}
-
-const testDetails = {
-  communal: 'только счётчики',
-  deposit: '55000₽',
-  commission: '15000₽'
-}
-
-const testParameters = {
-  rooms: '2',
-  area: '248 м2',
-  floor: '25/30'
-}
-
-const testPrice = '55000'
 
 export const LongPage = (): JSX.Element => {
   return (
@@ -46,11 +15,6 @@ export const LongPage = (): JSX.Element => {
           <h1>
             Долгосрочная аренда
           </h1>
-          <LongInfo adress={ testAdress }
-            details={ testDetails }
-            parameters={ testParameters }
-            price={ testPrice }
-          />
         </section>
       </main>
     </Layout>
