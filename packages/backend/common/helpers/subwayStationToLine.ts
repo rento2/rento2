45c8126896d2b7cd9ad126e59166ef05f9bbf9f2
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-04-15 00:24:08 +0300

    feat: add subway const and file of logic

---
+++ packages/backend/common/helpers/subwayStationToLine.ts
@@ -0,0 +1,12 @@
+import { SubwayStations } from './../constants/SubwayStations'
+
+export default function subwayStationToLine (subwayStation: string): string {
+  const subwayLine = SubwayStations.find(
+    (station) => station.title === subwayStation
+  )
+  if (subwayLine != null) {
+    return subwayLine.title
+  } else {
+    return 'These stations are not in our bases'
+  }
+}
