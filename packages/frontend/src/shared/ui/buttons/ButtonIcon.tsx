f15d6deae4927a5daaecc66f388534191aa82f66
Slavik <slava123101@gmail.com>
2022-04-24 21:14:52 +0500

    button with icon added

--- packages/frontend/src/shared/ui/buttons/ButtonIcon.tsx
+++ packages/frontend/src/shared/ui/buttons/ButtonIcon.tsx
@@ -1,35 +1,36 @@
-import classNames from "classnames"
-import styles from "./ButtonIcon.module.scss";
+import classNames from 'classnames'
+import styles from './ButtonIcon.module.scss'
 
 interface IButtonIcon {
-  icon?: JSX.Element
   children?: JSX.Element
   size: '40' | '32' | '24'
   classProps?: string
   onClick: () => void
-  grade: 'iris' | 'neutral'
-  iconFill?: boolean
+  grade?: 'iris' | 'neutral' // цвет кнопки с иконкой (пока только iris)
+  full: 'filled' | 'stroke' // должна ли иконка быть заполнена или только контуром
   disabled?: boolean
+  background?: 'iris' | 'none' // бэкграунд кнопки
 }
 
-export function ButtonIcon ({ icon, children, size, classProps, onClick, grade, iconFill, disabled }: IButtonIcon): JSX.Element {
-  const stylesColor = styles[`grade-${grade}`] ?? ''
+export function ButtonIcon ({ children, size, classProps, onClick, grade = 'iris', full, disabled, background = 'none' }: IButtonIcon): JSX.Element {
+  const stylesColor = styles[`grade-${grade}-full-${full}`] ?? ''
   const stylesSize = styles[`size-${size}`] ?? ''
+  const stylesBackground = styles[`bg-${background}-full-${full}`] ?? ''
 
   return (
-    <button className={
+    <button
+      className={
       classNames(
         classProps,
         styles['button-icon'],
         stylesSize,
-        stylesColor
-      )
-
-    }
-      disabled={disabled}
-      onClick={onClick}
+        stylesColor,
+        stylesBackground
+      ) }
+      disabled={ disabled }
+      onClick={ onClick }
     >
-      {icon || children}
+      {children}
     </button>
   )
 }
