3b5c86cbff4399dd7f80441b3b030a61c3ae9db5
Murashev Andrey <shshk777@yandex.ru>
2022-05-04 19:03:49 +0500

    contract text and layout added

--- packages/frontend/src/pages-fs/docs/agreements-short/ui/AgreementShortList.tsx
+++ packages/frontend/src/pages-fs/docs/agreements-short/ui/AgreementShortList.tsx
@@ -3,7 +3,9 @@ import classNames from 'classnames'
 import { agreementShortData } from '../model/agreementShortData'
 import { DocsShortModalPrice } from '@widgets/docs-short-modal-price'
 import styles from './AgreementShortList.module.scss'
-import { useModal } from '@shared/ui'
+import { DocumentsLayout, useModal } from '@shared/ui'
+import { useWindowDimensions } from '@shared/lib'
+import { SideMenu, HeadMenu } from '@widgets/documents-side-menu'
 
 // TODO
 // Работает через контекст. Контекст используется в кнопке AgreementShortButton, т.к. не хотела пробрасывать метод для открывания модалки через все дерево. Но може это решение не очень будут. Нужно будет смотреть, что получится, когда добавится аккордион
@@ -12,6 +14,7 @@ import { useModal } from '@shared/ui'
 export const AgreementShortList = (): JSX.Element => {
   const [isShownPriceDamage, setShownPriceDamage] = useModal()
   const [isShownRules, setShownRules] = useModal()
+  const { widthWindow } = useWindowDimensions()
 
   return (
     <DocsListContext.Provider value={ { setShownPriceDamage, setShownRules } }>
@@ -20,7 +23,15 @@ export const AgreementShortList = (): JSX.Element => {
         'container'
       ) }
       >
-        <DocsList items={ agreementShortData } />
+        <DocumentsLayout headMenu={ <HeadMenu /> }
+          sideMenu={ <SideMenu /> }
+          titleText="Частые вопросы"
+          widthProps={ 1059 }
+          widthWindowProps={ widthWindow }
+        >
+          <DocsList items={ agreementShortData } />
+        </DocumentsLayout>
+
         <DocsShortModalPrice hideProps={ setShownRules }
           isShownProps={ isShownRules }
         />
