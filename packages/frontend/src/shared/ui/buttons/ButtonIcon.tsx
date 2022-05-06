4f1a8bfc7da8932ac95c263df966410eaa57d535
Slavik <slava123101@gmail.com>
2022-05-06 22:58:09 +0500

    gallery slider added

--- packages/frontend/src/shared/ui/buttons/ButtonIcon.tsx
+++ packages/frontend/src/shared/ui/buttons/ButtonIcon.tsx
@@ -1,25 +1,28 @@
 import classNames from 'classnames'
+import { LegacyRef } from 'react'
 import styles from './ButtonIcon.module.scss'
 
 interface IButtonIcon {
   children?: JSX.Element
   size: '40' | '32' | '24'
   classProps?: string
-  onClick: () => void
+  onClick?: () => void
   grade?: 'iris' | 'neutral' // цвет кнопки с иконкой (пока только iris)
   full: 'filled' | 'stroke' // должна ли иконка быть заполнена или только контуром
   disabled?: boolean
   background?: 'iris' | 'none' // бэкграунд кнопки
+  refProp?: LegacyRef<HTMLButtonElement>
 }
 
 // Цвет передаваемых иконок может меняться за счет fill и stroke, Если цвет иконки меняется за счет stroke, то нужно передать full = stroke, иначе full = filled.
-export function ButtonIcon ({ children, size, classProps, onClick, grade = 'iris', full, disabled, background = 'none' }: IButtonIcon): JSX.Element {
+export function ButtonIcon ({ children, size, classProps, onClick, grade = 'iris', full, disabled, background = 'none', refProp }: IButtonIcon): JSX.Element {
   const stylesColor = styles[`grade-${grade}-full-${full}`] ?? ''
   const stylesSize = styles[`size-${size}`] ?? ''
   const stylesBackground = styles[`bg-${background}-full-${full}`] ?? ''
 
   return (
     <button
+      ref={ refProp }
       className={
       classNames(
         classProps,
