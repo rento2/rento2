47d826f176b63632c98b3659408f27d9130785c3
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-05-08 00:58:53 +0300

    feat: add description 'one' and 'list' metods

--- packages/backend/config/swagger.ts
+++ packages/backend/config/swagger.ts
@@ -1,40 +1,41 @@
 import { SwaggerConfig } from '@ioc:Adonis/Addons/Swagger'
 
+// eslint-disable-next-line
 export default {
-	uiEnabled: true, //disable or enable swaggerUi route
-	uiUrl: 'docs', // url path to swaggerUI
-	specEnabled: true, //disable or enable swagger.json route
-	specUrl: '/swagger.json',
+  uiEnabled: true, // disable or enable swaggerUi route
+  uiUrl: 'docs', // url path to swaggerUI
+  specEnabled: true, // disable or enable swagger.json route
+  specUrl: '/swagger.json',
 
-	middleware: [], // middlewares array, for protect your swagger docs and spec endpoints
+  middleware: [], // middlewares array, for protect your swagger docs and spec endpoints
 
-	options: {
-		definition: {
-			openapi: '3.0.0',
-			info: {
-				title: 'API Rento',
-				version: '1.0.0',
-				description: 'API Rento swagger docs'
-			},
-		components: {
-			securitySchemes: {
-			bearerAuth: {
-				type: "http",
-				scheme: "bearer",
-				bearerFormat: "JWT"
-			}
-			}
-		}
-		},
+  options: {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'API Rento',
+        version: '1.0.0',
+        description: 'API Rento swagger docs'
+      },
+      components: {
+        securitySchemes: {
+          bearerAuth: {
+            type: 'http',
+            scheme: 'bearer',
+            bearerFormat: 'JWT'
+          }
+        }
+      }
+    },
 
-		apis: [
-			'app/**/*.ts',
-			'docs/swagger/**/*.yml',
+    apis: [
+      'app/**/*.ts',
+      'docs/swagger/**/*.yml',
       'docs/swagger/*.yml',
-			'start/routes.ts'
-		],
-		basePath: '/api/v1/'
-	},
-	mode: process.env['NODE_ENV'] === 'production' ? 'PRODUCTION' : 'RUNTIME',
+      'start/routes.ts'
+    ],
+    basePath: '/api/v1/'
+  },
+  mode: process.env['NODE_ENV'] === 'production' ? 'PRODUCTION' : 'RUNTIME',
   specFilePath: 'docs/swagger.json'
 } as SwaggerConfig
