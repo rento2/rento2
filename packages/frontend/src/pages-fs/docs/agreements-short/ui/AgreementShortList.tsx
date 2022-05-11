f18c3c2a3fd51735a27ec93889b2611e24e8d981
Natalya <natasha-vd@yandex.ru>
2022-05-11 23:12:55 +0300

    front: small change

--- packages/frontend/src/pages-fs/docs/agreements-short/ui/AgreementShortList.tsx
+++ packages/frontend/src/pages-fs/docs/agreements-short/ui/AgreementShortList.tsx
@@ -1,15 +1,9 @@
 import { DocsList, DocsListContext } from '@widgets/docs-list'
-import classNames from 'classnames'
 import { agreementShortData } from '../model/agreementShortData'
 import { DocsShortModalPrice } from '@widgets/docs-short-modal-price'
-import styles from './AgreementShortList.module.scss'
 import { DocumentsLayout, useModal } from '@shared/ui'
 import { useWindowDimensions } from '@shared/lib'
 import { SideMenu, HeadMenu } from '@widgets/documents-side-menu'
-
-// TODO
-// Работает через контекст. Контекст используется в кнопке AgreementShortButton, т.к. не хотела пробрасывать метод для открывания модалки через все дерево. Но може это решение не очень будут. Нужно будет смотреть, что получится, когда добавится аккордион
-// Передавать компонент, который управляет аккордионом или же написать хук (???) Кнопка для аккордиона, если type = title
 
 export const AgreementShortList = (): JSX.Element => {
   const [isShownPriceDamage, setShownPriceDamage] = useModal()
@@ -18,11 +12,7 @@ export const AgreementShortList = (): JSX.Element => {
 
   return (
     <DocsListContext.Provider value={ { setShownPriceDamage, setShownRules } }>
-      <article className={ classNames(
-        styles['document__page'],
-        'container'
-      ) }
-      >
+      <article>
         <DocumentsLayout headMenu={ <HeadMenu /> }
           sideMenu={ <SideMenu /> }
           titleText="Частые вопросы"
