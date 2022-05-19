59db383d8f75138f3f2dadcd7e6bdfad8b0e26f0
Slavik <slava123101@gmail.com>
2022-05-19 14:07:36 +0500

    switch component renamed, renamed classes with BEM

---
+++ packages/frontend/src/shared/ui/switches/SwitchBase.tsx
@@ -0,0 +1,45 @@
+import classNames from 'classnames'
+import styles from './SwitchBase.module.scss'
+
+interface ISwitchBase {
+  name: string
+  labelText?: string // текст лейбла рядом с переключателем
+  labelTextPlacement?: 'right' | 'left' // расположение текстового лейбла относительно свича
+  containerClass?: string
+}
+
+export function SwitchBase ({ name, labelText, labelTextPlacement = 'right', containerClass }: ISwitchBase): JSX.Element {
+  const labelStyles = styles[`switch__text-label-${labelTextPlacement}`]
+
+  const textLabel = labelText ? (
+    <label
+      className={ labelStyles }
+      htmlFor={ name }
+    >
+      {labelText}
+    </label>
+  ) : null
+
+  return (
+    <div className={ classNames(styles['switch'], containerClass) }>
+      {labelTextPlacement === 'left' && textLabel}
+
+      <div className={ classNames(styles['switch__toggle']) }>
+        <input
+          className={ classNames(styles['switch__hidden-checkbox']) }
+          id={ name }
+          name={ name }
+          type="checkbox"
+        />
+        <label
+          className={ classNames(styles['switch__inner'], styles['inner']) }
+          htmlFor={ name }
+        >
+          <span className={ classNames(styles['switch__dot']) } />
+        </label>
+      </div>
+
+      {labelTextPlacement === 'right' && textLabel}
+    </div>
+  )
+}
