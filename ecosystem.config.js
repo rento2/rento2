068540f60c85872fb5f4cf86c636198d5ccbf1cf
Luka Nikitin <lukentuichennel@gmail.com>
2022-06-04 22:31:28 +0200

    fix ecosystem fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,5 +5,13 @@ module.exports = {
     instances: 'max',
     exec_mode: 'cluster',
     autorestart: true
+  },
+  {
+    name: 'rento-frontend',
+    script: 'npm',
+    args: 'start',
+    cwd: '/var/www/rento/packages/frontend',
+    instances: 'max',
+    autorestart: true
   }]
 }
