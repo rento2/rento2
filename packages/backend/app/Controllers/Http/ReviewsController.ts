62ec5cb525ab8d80403bc051a6cca65f4876b902
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-07-16 01:05:55 +0300

    feat: add LIST method

--- packages/backend/app/Controllers/Http/ReviewsController.ts
+++ packages/backend/app/Controllers/Http/ReviewsController.ts
@@ -1,27 +1,29 @@
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { HttpStatusCode } from '../../../common/constants/HttpStatusCode'
+import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import { HttpStatusCode } from "../../../common/constants/HttpStatusCode";
 import {
   creatingErrMsg,
   creatingOkMsg,
   creatingPaginatedList,
-} from '../../../common/helpers/creatingResponse'
-import Review from 'App/Models/Review'
-import CreateReviewValidator from 'App/Validators/ReviewValidator'
-import { schema } from '@ioc:Adonis/Core/Validator'
-import Apartment from 'App/Models/Apartment'
+} from "../../../common/helpers/creatingResponse";
+import Review from "App/Models/Review";
+import CreateReviewValidator from "App/Validators/ReviewValidator";
+import { schema } from "@ioc:Adonis/Core/Validator";
+import Apartment from "App/Models/Apartment";
 
 export default class ReviewsController {
-  public async list ({ response, request }: HttpContextContract): Promise<void> {
-    const { sortDirection } = request.qs()
+  public async list({ response, request }: HttpContextContract): Promise<void> {
+    const { sortDirection } = request.qs();
     const { search } = await request.validate({
       schema: schema.create({
         search: schema.string.optional(),
       }),
-    })
+    });
 
-    let reviews = Review.query().preload('apartment')
+    let reviews = Review.query().preload("apartment", (apartmentQuery) => {
+      apartmentQuery.preload("photos");
+    });
     if (search) {
-      reviews = reviews.andWhere('author', 'ilike', `%${search}%`)
+      reviews = reviews.andWhere("author", "ilike", `%${search}%`);
     }
 
     return response
@@ -29,83 +31,83 @@ export default class ReviewsController {
       .send(
         creatingPaginatedList(
           await reviews
-            .orderBy('createdAt', sortDirection === 'asc' ? 'asc' : 'desc')
-            .paginate(request.param('page', 1))
+            .orderBy("createdAt", sortDirection === "asc" ? "asc" : "desc")
+            .paginate(request.param("page", 1))
         )
-      )
+      );
   }
 
-  public async one ({ response, request }: HttpContextContract): Promise<void> {
-    const review = await Review.find(request.param('id', null))
+  public async one({ response, request }: HttpContextContract): Promise<void> {
+    const review = await Review.find(request.param("id", null));
 
     if (!review) {
       return response
         .status(HttpStatusCode.NotFound)
-        .send(creatingErrMsg('error', 'Review not found'))
+        .send(creatingErrMsg("error", "Review not found"));
     }
 
     const apartment = await Apartment.query()
-      .preload('photos')
-      .where('id', review.apartmentId)
-      .first()
+      .preload("photos")
+      .where("id", review.apartmentId)
+      .first();
 
-    let apartmenBody = {}
+    let apartmenBody = {};
 
     if (apartment) {
       const photos = apartment.photos.map(
         (photo) => `${photo.link}${photo.path}`
-      )
+      );
       apartmenBody = {
         name: apartment.name,
         photos,
-      }
+      };
     }
 
     return response
       .status(HttpStatusCode.OK)
-      .send(creatingOkMsg({ review, apartment: apartmenBody }))
+      .send(creatingOkMsg({ review, apartment: apartmenBody }));
   }
 
-  public async update ({
+  public async update({
     response,
     request,
   }: HttpContextContract): Promise<void> {
-    const review = await Review.find(request.param('id', null))
+    const review = await Review.find(request.param("id", null));
     if (!review) {
-      return response.send(creatingErrMsg('error', 'Review not found'))
+      return response.send(creatingErrMsg("error", "Review not found"));
     }
 
     const updatedReview = await review
       .merge(await request.validate(CreateReviewValidator))
-      .save()
+      .save();
 
     return response
       .status(HttpStatusCode.OK)
-      .send(creatingOkMsg(updatedReview))
+      .send(creatingOkMsg(updatedReview));
   }
 
-  public async create ({
+  public async create({
     request,
     response,
   }: HttpContextContract): Promise<void> {
     const review = await Review.create({
       ...(await request.validate(CreateReviewValidator)),
       isApproved: false,
-    })
+    });
 
-    return response.status(HttpStatusCode.Created).send(creatingOkMsg(review))
+    return response.status(HttpStatusCode.Created).send(creatingOkMsg(review));
   }
 
-  public async delete ({
+  public async delete({
     response,
     request,
   }: HttpContextContract): Promise<any> {
-    const review = await Review.find(request.param('id', null))
+    const review = await Review.find(request.param("id", null));
     if (!review) {
-      return response.send(creatingErrMsg('error', 'Review not found'))
+      return response.send(creatingErrMsg("error", "Review not found"));
     }
 
-    await review.delete()
-    return response.status(HttpStatusCode.OK).send(creatingOkMsg(review.id))
+    await review.delete();
+    return response.status(HttpStatusCode.OK).send(creatingOkMsg(review.id));
   }
 }
