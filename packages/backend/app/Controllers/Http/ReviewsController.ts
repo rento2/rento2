03372ca7286ed79ee22e42566cbe65c981ddda34
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-07-16 00:57:44 +0300

    feat: add ONE method

--- packages/backend/app/Controllers/Http/ReviewsController.ts
+++ packages/backend/app/Controllers/Http/ReviewsController.ts
@@ -1,27 +1,32 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { HttpStatusCode } from '../../../common/constants/HttpStatusCode'
-import { creatingErrMsg, creatingOkMsg, creatingPaginatedList } from '../../../common/helpers/creatingResponse'
+import {
+  creatingErrMsg,
+  creatingOkMsg,
+  creatingPaginatedList,
+} from '../../../common/helpers/creatingResponse'
 import Review from 'App/Models/Review'
 import CreateReviewValidator from 'App/Validators/ReviewValidator'
 import { schema } from '@ioc:Adonis/Core/Validator'
+import Apartment from 'App/Models/Apartment'
 
 export default class ReviewsController {
   public async list ({ response, request }: HttpContextContract): Promise<void> {
     const { sortDirection } = request.qs()
     const { search } = await request.validate({
       schema: schema.create({
-        search: schema.string.optional()
-      })
+        search: schema.string.optional(),
+      }),
     })
 
     let reviews = Review.query().preload('apartment')
     if (search) {
-      reviews = reviews
-        .andWhere('author', 'ilike', `%${search}%`)
+      reviews = reviews.andWhere('author', 'ilike', `%${search}%`)
     }
 
     return response
-      .status(HttpStatusCode.OK).send(
+      .status(HttpStatusCode.OK)
+      .send(
         creatingPaginatedList(
           await reviews
             .orderBy('createdAt', sortDirection === 'asc' ? 'asc' : 'desc')
@@ -32,38 +37,69 @@ export default class ReviewsController {
 
   public async one ({ response, request }: HttpContextContract): Promise<void> {
     const review = await Review.find(request.param('id', null))
+
     if (!review) {
-      return response.status(HttpStatusCode.NotFound).send(creatingErrMsg('error', 'Review not found'))
+      return response
+        .status(HttpStatusCode.NotFound)
+        .send(creatingErrMsg('error', 'Review not found'))
     }
 
-    return response.status(HttpStatusCode.OK).send(creatingOkMsg(review))
+    const apartment = await Apartment.query()
+      .preload('photos')
+      .where('id', review.apartmentId)
+      .first()
+
+    let apartmenBody = {}
+
+    if (apartment) {
+      const photos = apartment.photos.map(
+        (photo) => `${photo.link}${photo.path}`
+      )
+      apartmenBody = {
+        name: apartment.name,
+        photos,
+      }
+    }
+
+    return response
+      .status(HttpStatusCode.OK)
+      .send(creatingOkMsg({ review, apartment: apartmenBody }))
   }
 
-  public async update ({ response, request }: HttpContextContract): Promise<void> {
+  public async update ({
+    response,
+    request,
+  }: HttpContextContract): Promise<void> {
     const review = await Review.find(request.param('id', null))
     if (!review) {
       return response.send(creatingErrMsg('error', 'Review not found'))
     }
 
-    const updatedReview = await review.merge(
-      await request.validate(CreateReviewValidator)
-    ).save()
+    const updatedReview = await review
+      .merge(await request.validate(CreateReviewValidator))
+      .save()
 
-    return response.status(HttpStatusCode.OK).send(creatingOkMsg(updatedReview))
+    return response
+      .status(HttpStatusCode.OK)
+      .send(creatingOkMsg(updatedReview))
   }
 
-  public async create ({ request, response }: HttpContextContract): Promise<void> {
+  public async create ({
+    request,
+    response,
+  }: HttpContextContract): Promise<void> {
     const review = await Review.create({
       ...(await request.validate(CreateReviewValidator)),
       isApproved: false,
     })
 
-    return response
-      .status(HttpStatusCode.Created)
-      .send(creatingOkMsg(review))
+    return response.status(HttpStatusCode.Created).send(creatingOkMsg(review))
   }
 
-  public async delete ({ response, request }: HttpContextContract): Promise<any> {
+  public async delete ({
+    response,
+    request,
+  }: HttpContextContract): Promise<any> {
     const review = await Review.find(request.param('id', null))
     if (!review) {
       return response.send(creatingErrMsg('error', 'Review not found'))
