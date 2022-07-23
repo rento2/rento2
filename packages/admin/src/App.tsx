01f883eb5c3749e7d2371107688df9b93975f033
Luka Nikitin <lukentuichennel@gmail.com>
2022-07-23 15:43:25 +0200

    metro lines and stations

--- packages/admin/src/App.tsx
+++ packages/admin/src/App.tsx
@@ -1,9 +1,10 @@
-import { Admin, EditGuesser, ListGuesser, Resource } from 'react-admin'
+import { Admin, ListGuesser, Resource } from 'react-admin'
 import restProvider from './common/dataProvider'
 import PostIcon from '@material-ui/icons/Book'
-import BannerIcon from '@material-ui/icons/AirplanemodeActive'
+import ViewCarouselIcon from '@material-ui/icons/ViewCarousel'
 import BedIcon from '@material-ui/icons/KingBed'
 import RateReview from '@material-ui/icons/RateReview'
+import SubwayIcon from '@material-ui/icons/Subway'
 
 import { ApartmentList, ApartmentCreate, ApartmentEdit } from './components/Apartment'
 import { authProvider } from 'common'
@@ -12,6 +13,8 @@ import { httpClient } from 'common/httpClient'
 import { BannerCreate, BannerEdit, BannerList } from 'components/Banner'
 import { SleepingPlacesCreate, SleepingPlacesEdit, SleepingPlacesList } from 'components/SleepingPlaces'
 import { ReviewEdit, ReviewList } from 'components/Review'
+import { MetroLineCreate, MetroLineEdit, MetroLineList } from 'components/MetroLine'
+import { MetroStationCreate, MetroStationEdit, MetroStationList } from 'components/MetroStation'
 
 const dataProvider = restProvider(String(process.env['REACT_APP_SERVER_URL']), httpClient)
 
@@ -31,9 +34,25 @@ const App = (): JSX.Element => (
     <Resource
       create={BannerCreate}
       edit={BannerEdit}
-      icon={BannerIcon}
+      icon={ViewCarouselIcon}
       list={BannerList}
       name='banners'
+    />
+
+    <Resource
+      create={MetroLineCreate}
+      edit={MetroLineEdit}
+      icon={SubwayIcon}
+      list={MetroLineList}
+      name='metro-lines'
+    />
+
+    <Resource
+      create={MetroStationCreate}
+      edit={MetroStationEdit}
+      icon={SubwayIcon}
+      list={MetroStationList}
+      name='metro-stations'
     />
 
     <Resource
@@ -41,17 +60,6 @@ const App = (): JSX.Element => (
       icon={RateReview}
       list={ReviewList}
       name='reviews'
-    />
-
-    <Resource
-      list={ListGuesser}
-      name='metro-stations'
-    />
-
-    <Resource
-      edit={EditGuesser}
-      list={ListGuesser}
-      name='metro-lines'
     />
 
     <Resource
---
+++ packages/admin/src/components/MetroStation/index.ts
@@ -0,0 +1,3 @@
+export { default as MetroStationCreate } from './MetroStationCreate'
+export { default as MetroStationEdit } from './MetroStationEdit'
+export { default as MetroStationList } from './MetroStationList'
